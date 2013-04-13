fe029f5de0c21af7aa1b8cb47c1d706079f1d357
Stefano Valle <s.valle@mvassociati.it>
2013-04-13 16:27:20 +0200

    Add classes and views to manage entity CRUD

--- src/MvaModuleTemplate/Controller/IndexController.php
+++ src/MvaModuleTemplate/Controller/IndexController.php
@@ -7,8 +7,119 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+    
+    private $I_service;
+    private $I_form;
+    
+    public function __construct($I_service, $I_form) {
+        $this->I_service = $I_service;    
+        $this->I_form = $I_form;
+    }
+    
     public function indexAction()
     {
-        return new ViewModel();
+        return new ViewModel(array(
+            'aI_dogs' => $this->I_service->getAllDogs(),
+            'as_messages' => $this->flashMessenger()->setNamespace('dog')->getMessages(),
+        ));
     }
+    
+    public function newAction() 
+    {
+        // set form action
+        $this->I_form->setAttribute('action', '/mva-module-template/process');
+        
+        $I_view = new ViewModel(array('form' => $this->I_form, 'title' => 'New dog'));
+        $I_view->setTemplate('mva-module-template/index/dog-form');
+        return $I_view;
+    }
+    
+    public function editAction()
+    {
+        $I_dog = $this->getEntityFromQuerystring();
+        
+        // set form action
+        $this->I_form->setAttribute('action', '/mva-module-template/process');
+        
+        // bind entity values to form
+        $this->I_form->bind($I_dog);
+        
+        $I_view = new ViewModel(array('form' => $this->I_form, 'title' => 'Edit dog'));
+        $I_view->setTemplate('mva-module-template/index/dog-form');
+        return $I_view;
+    }
+    
+    public function deleteAction()
+    {
+        $I_dog = $this->getEntityFromQuerystring();
+                
+        $this->I_service->deleteDog($I_dog);
+        
+        return $this->redirect()->toRoute('mva-module-template');
+    }
+    
+    public function processAction(){
+        if ($this->request->isPost()) {
+
+            // get post data
+            $as_post = $this->request->getPost()->toArray();
+            
+            // fill form
+            $this->I_form->setData($as_post);
+    
+            // check if form is valid
+            if(!$this->I_form->isValid()) {
+                
+                // prepare view
+                $I_view = new ViewModel(array('form'  => $this->I_form,
+                                               'title' => 'Some errors during dog editing'));
+                $I_view->setTemplate('mva-module-template/index/dog-form');
+                return $I_view;
+                
+            }
+    
+            // use service to save data
+            $I_dog = $this->I_service->upsertDogFromArray($as_post);
+    
+            if ( $as_post['id'] > 0 ) {
+                $this->flashMessenger()->setNamespace('dog')->addMessage('Dog ' . $I_dog->getName() . ' updated successfully');
+            } else {
+                $this->flashMessenger()->setNamespace('dog')->addMessage('Dog' . $I_dog->getName() . ' inserted successfully');
+            }
+            
+            return $this->redirect()->toRoute('mva-module-template');
+    
+        }
+        
+        
+        $this->getResponse()->setStatusCode(404);
+        return;
+    }
+    
+    
+    /*
+     * Private methods
+     */
+    
+    private function getEntityFromQuerystring() {
+
+        $i_id = (int)$this->params('id');
+        
+        if (empty($i_id) || $i_id <= 0){
+            $this->getResponse()->setStatusCode(404);    //@todo there is a better way?
+                                                         // Probably triggering Not Found Event SM
+                                                         // Zend\Mvc\Application: dispatch.error 
+            return;
+        }
+        
+        $I_entity = $this->I_service->getEntity($i_id);
+        if ($I_entity === null ){
+            $this->getResponse()->setStatusCode(404);    //@todo there is a better way?
+            return;
+        }
+        
+        return $I_entity;
+        
+    }
+    
 }
---
+++ src/MvaModuleTemplate/Service/DogService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace MvaModuleTemplate\Service;
+
+use \MvaModuleTemplate\Entity\Dog;
+
+class DogService {
+    
+    private $I_dogRepository;
+    private $I_entityManager;
+    
+    public function __construct($I_entityManager) {
+        $this->I_dogRepository  = $I_entityManager->getRepository('MvaModuleTemplate\Entity\Dog');
+        $this->I_entityManager = $I_entityManager;
+    }
+    
+    public function getAllDogs(){
+        return $this->I_dogRepository->findAll();
+    }
+    
+    public function getEntity($i_id){
+        return $this->I_dogRepository->find($i_id);
+    }
+    
+    /**
+     * Inserts or updates Dog
+     * 
+     * @param array $am_formData
+     * @return Ambigous <NULL, \MvaModuleTemplate\Entity\Dog>
+     */
+    public function upsertDogFromArray(array $am_formData){
+
+        $I_dog = NULL;
+        if (isset($am_formData['id']) && 
+            is_numeric($am_formData['id']) && 
+            $am_formData['id'] > 0){
+            $I_dog = $this->getEntity($am_formData['id']);
+        }
+
+        if (!($I_dog instanceof Dog)) {
+            $I_dog = new Dog();
+        }
+
+        $I_dog->fillWith($am_formData);
+        
+        $this->I_entityManager->persist($I_dog);
+        $this->I_entityManager->flush();
+    
+        return $I_dog;
+    }
+    
+    public function deleteDog(Dog $I_dog) {
+        $this->I_entityManager->remove($I_dog);
+        $this->I_entityManager->flush();
+    }    
+    
+}
